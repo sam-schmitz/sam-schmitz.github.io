f4040cfc181e73420a012d11d985793ba727fbc6
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2023-08-08 18:10:19 -0500

    made some cosmetic changes

--- src/App.js
+++ src/App.js
@@ -39,15 +39,23 @@ export const PROJECTS = [
 
 function ExpandedProject({ activeProject }) {
     //make the Github Link an active link
-    return (
-        <div>
-            <h5>{activeProject.name}</h5>
-            <p><b>Languages: </b>{activeProject.languages}</p>
-            <p><b>Libraries: </b>{activeProject.libraries}</p>
-            <p><b>Decription: </b>{activeProject.description}</p>
-            <p><b>Github Link: </b><a href={activeProject.link}>{activeProject.link}</a></p>
-        </div>
+    if (activeProject == 'None') {
+        return (
+            <div>
+                <p>Select a project to learn more</p>
+            </div>
         );
+    } else {
+        return (
+            <div>
+                <h5>{activeProject.name}</h5>
+                <p><b>Languages: </b>{activeProject.languages.join(', ')}</p>
+                <p><b>Libraries: </b>{activeProject.libraries.join(', ')}</p>
+                <p><b>Decription: </b>{activeProject.description}</p>
+                <p><b>Github Link: </b><a href={activeProject.link}>{activeProject.link}</a></p>
+            </div>
+        );
+    }
 }
 
 function ProjectRow({ project, active, onActiveProjectChange }) {
@@ -56,10 +64,13 @@ function ProjectRow({ project, active, onActiveProjectChange }) {
             {project.name}
         </span>;
 
-    //add in a highlight if project is being expanded
+    const c = "";
+    if (active) {
+        const c = "table-active";
+    }
 
     return (
-        <tr>
+        <tr class={c}>
             <td>
                 <input
                     type="checkbox"
@@ -68,8 +79,8 @@ function ProjectRow({ project, active, onActiveProjectChange }) {
                 />
             </td>
             <td>{name}</td>
-            <td>{project.languages}</td>
-            <td>{project.libraries}</td>
+            <td>{project.languages.join(', ')}</td>
+            <td>{project.libraries.join(', ')}</td>
         </tr>
         );
 }
@@ -118,6 +129,7 @@ function SearchBar({ filterText, finishedOnly, onFilterTextChange, onFinishedOnl
                 type="text"
                 value={filterText} placeholders="Search..."
                 onChange={(e) => onFilterTextChange(e.target.value)} />
+            {'  '}
             <label>
                 <input
                     type="checkbox"
@@ -156,25 +168,30 @@ function Header() {
     return (
         <div class="text-light">
             <h1>Portfolio</h1>
+            <hr></hr>
         </div>
     );
 }
 
 function Body() {
-    //define expand, if nothing is selected return none else return the expandedRow of that
-    const [activeProject, setActiveProject] = useState('');
+    const [activeProject, setActiveProject] = useState('None');
 
     return (
-        <div class='row text-light'>
-            <div class="col-sm-6">
-                <FilterableProjectTable
-                    projects={PROJECTS}
-                    activeProject={activeProject}
-                    setActiveProject={setActiveProject} />
+        <div>
+            <div class="row text-light">
+                <h3>Projects</h3>
             </div>
-            <div class="col-sm-6">
-                <ExpandedProject
-                    activeProject={activeProject} />
+            <div class='row text-light'>
+                <div class="col-md-6">
+                    <FilterableProjectTable
+                        projects={PROJECTS}
+                        activeProject={activeProject}
+                        setActiveProject={setActiveProject} />
+                </div>
+                <div class="col-md-6">
+                    <ExpandedProject
+                        activeProject={activeProject} />
+                </div>
             </div>
         </div>
     );
@@ -182,7 +199,7 @@ function Body() {
 
 function App() {
     return (
-        <div class="container-fluid bg-dark w-100 p-3">
+        <div class="container-fluid bg-dark p-3 min-vh-100">
             <Header />
             <Body />
         </div>
